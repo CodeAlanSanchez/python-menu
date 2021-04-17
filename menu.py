a2d56d89b2485eed6accad1344e6cae97d92801d
Alan Sanchez <realalansan@gmail.com>
2021-04-16 18:37:31 -0700

    Add show and add item functionality

--- menu.py
+++ menu.py
@@ -1,5 +1,5 @@
 import math
-import item
+from item import Item
 
 class Menu():
 
@@ -18,9 +18,10 @@ class Menu():
         print("2. Add Item")
         print("3. Edit Item\n")
         print("4. Quit")
-        ans = self.validate_input(input("\n> "))
+        ans = self.validate_input(input("\n> "), 4)
         if ans == 1:
-            pass
+            self.add_item(self.input_item())
+            self.show_menu()
         if ans == 2:
             pass
         if ans == 3:
@@ -35,3 +36,32 @@ class Menu():
                 return ans
         except:
             return self.validate_input("Please input a valid option...\n> ")
+
+    def input_item(self):
+        name = input("\nInput the item name: \n\n> ")
+        amount = input("\nInput the amount of items: \n\n> ")
+        price = input("\nInput the price of each item: \n\n> $")
+        return Item(name, amount, price)
+
+    def add_item(self, item):
+        for i in self.list_of_items:
+            if i.get_name().lower() == item.get_name().lower() and i.get_price() == item.get_price():
+                i.set_amount(i.get_amount + item.get_amount())
+                return
+        self.list_of_items.append(item)
+    
+    def show_items(self):
+        iteration = 0
+        total = 0
+        if len(self.list_of_items) <= 0:
+            print("\nList is empty...")
+            input("\nPress enter to continue...")
+            return
+        print()
+        for i in self.list_of_items:
+            print("{}. {} {} (${price_per:.2f} per): ${price_total:.2f}".format(str(iteration+1), i.get_amount(),
+                                                                                i.get_name(), price_per=i.get_price_per()/100, price_total=i.get_price_total()/100))
+            total = total + i.get_total_price()
+            iteration = iteration + 1
+        print("\nTotal: ${total:.2f}".format(total=total/100))
+        input("\nPress enter to continue...")
